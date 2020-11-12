0f2c5c9a4cc94aeba47923833f7b70a59f048ac9
nkawaller <63615939+nkawaller@users.noreply.github.com>
2020-11-12 09:53:01 -0500

    set up social authentication

--- movies_project/settings.py
+++ movies_project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
     'accounts',
     'pages'
 ]
